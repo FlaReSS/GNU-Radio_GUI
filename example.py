6632bfb103b4a083afe55d6408ebc5cbdb03b669
MVGavrilov <volervagashim@gmail.com>
2019-08-26 15:46:45 +0200

    first commit

---
+++ example.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Example
+# Generated: Mon Aug 26 15:23:46 2019
+##################################################
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt5 import Qt
+from PyQt5 import Qt, QtCore
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio import zeromq
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import SimpleXMLRPCServer
+import sip
+import sys
+import threading
+from gnuradio import qtgui
+
+
+class example(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Example")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Example")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "example")
+        self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 16000
+        self.myvar = myvar = 1000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_float, 1, 'tcp://127.0.0.1:5556', 1000, False, -1)
+        self.xmlrpc_server_0 = SimpleXMLRPCServer.SimpleXMLRPCServer(('localhost', 8080), allow_none=True)
+        self.xmlrpc_server_0.register_instance(self)
+        self.xmlrpc_server_0_thread = threading.Thread(target=self.xmlrpc_server_0.serve_forever)
+        self.xmlrpc_server_0_thread.daemon = True
+        self.xmlrpc_server_0_thread.start()
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	myvar, #size
+        	samp_rate, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-2.5, 2.5)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, myvar, 1, 0)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.zeromq_pub_sink_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "example")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_myvar(self):
+        return self.myvar
+
+    def set_myvar(self, myvar):
+        self.myvar = myvar
+        self.analog_sig_source_x_0.set_frequency(self.myvar)
+
+
+def main(top_block_cls=example, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
---
+++ gui.py
@@ -0,0 +1,238 @@
+import sys
+import socket
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import xmlrpclib
+global xmlrpc_client_0
+xmlrpc_client_0 = xmlrpclib.ServerProxy('http://localhost:8080')
+
+global varList
+varList = []
+global mainflag
+mainflag = False
+
+def opencfg():
+	try:
+		with open('var.cfg', 'r') as f:
+			for line in f:
+				l = line.split()
+				name = l[0]
+				vtype = l[1]
+				defv = l[2]
+				VariableWidget(name, vtype, defv)
+			varSort()
+	except socket.error:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Warning)
+			msg.setText("Connection error!")
+			msg.setWindowTitle("Error!")
+			retval = msg.exec_()
+			exit()
+	except:
+		pass
+
+def savecfg():
+	global varList
+	with open('var.cfg', 'w') as f:
+		for i in varList:
+			f.write(i.name + ' ' + i.vtype + ' ' + i.defaultv + '\n')
+
+
+class VariableWidget(QWidget):
+	"""docstring for ClassName"""
+	def __init__(self, name, vtype, defaultv, realval = 0):
+		super(VariableWidget, self).__init__()
+		self.name = name
+		self.vtype = vtype
+		self.defaultv = defaultv
+		self.realval = realval
+		self.setLayout(QGridLayout())
+		self.lineEdit = QLineEdit(defaultv)
+		self.lineEdit.returnPressed.connect(self._setValue)
+		self.displ = QLineEdit(str(realval))
+		self.displ.setReadOnly(True)
+		self.label = QLabel(name + ':' + vtype)
+		self.btn = QToolButton()
+		myicon = QIcon()
+		myicon.addPixmap(QPixmap("./trash-can.png"), QIcon.Normal, QIcon.Off)
+		self.btn.setIcon(myicon)
+		self.btn.setIconSize(QSize(24, 24))
+		self.btn.setToolButtonStyle(Qt.ToolButtonIconOnly)
+		self.layout().addWidget(self.label, 0, 0)
+		self.layout().addWidget(self.displ, 0, 1)
+		self.layout().addWidget(self.lineEdit, 0, 2)
+		self.layout().addWidget(self.btn, 0, 3)
+		self.btn.clicked.connect(self._rmv)
+		global xmlrpc_client_0
+		tmp = str('get_'+self.name)
+		self.getter = getattr(xmlrpc_client_0, tmp)
+		tmp = str('set_'+self.name)
+		self.setter = getattr(xmlrpc_client_0, tmp)
+		try:
+			self._updateVal()
+		except xmlrpclib.Fault:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Warning)
+			msg.setText("Error! Perhaps a variable with '%s' name does not exist."%self.name)
+			msg.setWindowTitle("Warning!")
+			retval = msg.exec_()
+			return			
+			
+		global varList
+		varList.append(self)
+	def _rmv(self, idkwtf):
+		global varList
+		varList.remove(self)
+		self.close()
+		varSort()
+
+	def _updateVal(self):
+		self.displ.setText(str(self.getter()))
+
+	def _setValue(self):
+		try:
+			if self.vtype == 'float':
+				self.setter(float(self.lineEdit.text()))
+			else:
+				self.setter(int(self.lineEdit.text()))
+			self._updateVal()
+		except socket.error:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Warning)
+			msg.setText("Connection error!")
+			msg.setWindowTitle("Error!")
+			retval = msg.exec_()
+		except ValueError:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Warning)
+			msg.setText("Wrong value\nIt should be %s"%self.vtype)
+			msg.setWindowTitle("Error!")
+			retval = msg.exec_()
+
+app = QApplication(sys.argv)
+
+app.aboutToQuit.connect(savecfg)
+
+mainWindow = QWidget()
+mainGrid = QGridLayout()
+mainWindow.setLayout(mainGrid)
+mainWindow.setGeometry(100,100,800,600)
+mainWindow.setWindowTitle("GUI test")
+
+
+fileWidget = QWidget()
+fileGrid = QGridLayout()
+fileWidget.setLayout(fileGrid)
+
+openLogBtn = QPushButton()
+openLogBtn.setText("Open")
+
+def olb_clicked():
+	name = QFileDialog.getSaveFileName()
+	openLogLE.setText(name)
+
+openLogBtn.clicked.connect(olb_clicked)
+
+openLogLE = QLineEdit()
+openLogLE.setPlaceholderText("Log filename (output)")
+openLogLE.setReadOnly(True)
+openLogLE.setFocusPolicy(0)
+
+fileGrid.addWidget(openLogLE, 0, 0, 1, 4)
+fileGrid.addWidget(openLogBtn, 0, 5, 1, 1)
+
+openInBtn = QPushButton()
+openInBtn.setText("Open")
+
+def oib_clicked():
+	name = QFileDialog.getSaveFileName()
+	openInLE.setText(name)
+
+
+openInBtn.clicked.connect(oib_clicked)
+
+
+openInLE = QLineEdit()
+openInLE.setPlaceholderText("Data filename (input)")
+openInLE.setReadOnly(True)
+openInLE.setFocusPolicy(0)
+
+fileGrid.addWidget(openInLE, 1, 0, 1, 4)
+fileGrid.addWidget(openInBtn, 1, 5, 1, 1)
+
+
+
+
+addWidget = QWidget()
+addGrid = QGridLayout()
+addWidget.setLayout(addGrid)
+
+addNameLE = QLineEdit()
+addNameLE.setPlaceholderText("Name")
+
+addValLE = QLineEdit()
+addValLE.setPlaceholderText("Value")
+
+addCB = QComboBox()
+addCB.addItem('float')
+addCB.addItem('int')
+addCB.addItem('short')
+addCB.addItem('byte')
+
+addBtn = QPushButton()
+addBtn.setText("Add")
+
+
+def testAdd():
+	if addCB.currentText() == 'float':
+		try:
+			val = float(addValLE.text())
+		except ValueError:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Warning)
+			msg.setText("That is not a float!")
+			msg.setWindowTitle("Warning!")
+			retval = msg.exec_()
+			return
+	else:
+		try:
+			val = int(addValLE.text())
+		except ValueError:
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Warning)
+			msg.setText("That is not a number!")
+			msg.setWindowTitle("Warning!")
+			retval = msg.exec_()
+			return
+	VariableWidget(addNameLE.text(), addCB.currentText(), addValLE.text())
+	varSort()
+
+addBtn.clicked.connect(testAdd)
+addNameLE.returnPressed.connect(testAdd)
+addValLE.returnPressed.connect(testAdd)
+
+addGrid.addWidget(addNameLE, 0, 0, 1, 1)
+addGrid.addWidget(addCB, 0, 1, 1, 1)
+addGrid.addWidget(addValLE, 0, 2, 1, 1)
+addGrid.addWidget(addBtn, 0, 3, 1, 1)
+
+
+varWidget = QWidget()
+varGrid = QGridLayout()
+# varGrid.addLayout(varGrid, 2,2)
+varWidget.setLayout(varGrid)
+
+
+def varSort():
+	for i in range(len(varList)):
+		varGrid.addWidget(varList[i], i/2, i%2)
+
+mainGrid.addWidget(addWidget, 0, 0, 1, 1)
+mainGrid.addWidget(varWidget, 1, 0, 10, 1)
+mainGrid.addWidget(fileWidget, 12, 0, 1, 1)
+
+opencfg()
+
+mainWindow.show()
+
+sys.exit(app.exec_())
